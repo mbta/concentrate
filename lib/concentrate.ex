c5db0feb3e17d764599758ecba041b80b93dafe6
Paul Swartz <paulswartz@gmail.com>
2017-12-27 08:14:19 -0500

    allow runtime configuration via JSON
    By putting a JSON configuration file in CONCENTRATE_JSON, we can update the
    configuration at startup time.

--- lib/concentrate.ex
+++ lib/concentrate.ex
@@ -5,6 +5,73 @@ defmodule Concentrate do
   use Application
 
   def start(_type, _args) do
+    "CONCENTRATE_JSON"
+    |> System.get_env()
+    |> parse_json_configuration
+    |> Enum.each(&update_configuration/1)
+
     Concentrate.Supervisor.start_link()
+  end
+
+  def parse_json_configuration(nil) do
+    []
+  end
+
+  def parse_json_configuration(json) do
+    json = Poison.decode!(json)
+    Enum.flat_map(json, &decode_json_key_value/1)
+  end
+
+  defp decode_json_key_value({"sources", source_object}) do
+    realtime = source_object["gtfs_realtime"] || %{}
+    enhanced = source_object["gtfs_realtime_enhanced"] || %{}
+
+    [
+      sources: [
+        gtfs_realtime: atomize_keys(realtime),
+        gtfs_realtime_enhanced: atomize_keys(enhanced)
+      ]
+    ]
+  end
+
+  defp decode_json_key_value({"gtfs", object}) do
+    if url = object["url"] do
+      [gtfs: [url: url]]
+    else
+      []
+    end
+  end
+
+  defp decode_json_key_value({"sinks", sinks_object}) do
+    sinks =
+      if s3_object = sinks_object["s3"] do
+        %{
+          s3: [
+            bucket: s3_object["bucket"],
+            prefix: s3_object["prefix"]
+          ]
+        }
+      else
+        %{}
+      end
+
+    [
+      sinks: sinks
+    ]
+  end
+
+  defp decode_json_key_value(_unknown) do
+    []
+  end
+
+  defp atomize_keys(enumerable) do
+    # UNSAFE! only call this during startup, and with controlled JSON files.
+    for {key, value} <- enumerable, into: %{} do
+      {String.to_atom(key), value}
+    end
+  end
+
+  defp update_configuration({key, value}) do
+    Application.put_env(:concentrate, key, value)
   end
 end
---
+++ test/concentrate_test.exs
@@ -0,0 +1,67 @@
+defmodule ConcentrateTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  import Concentrate
+
+  describe "parse_json_configuration/1" do
+    test "nil return empty list" do
+      assert parse_json_configuration(nil) == []
+    end
+
+    test "invalid JSON raises an exception" do
+      assert_raise Poison.SyntaxError, fn -> parse_json_configuration("{") end
+    end
+
+    test "parses config into a keyword list" do
+      body = ~s(
+{
+  "sources": {
+    "gtfs_realtime": {
+      "name_1": "url_1",
+      "name_2": "url_2"
+    },
+    "gtfs_realtime_enhanced": {
+      "enhanced_1": "url_3"
+    }
+  },
+  "gtfs": {
+    "url": "gtfs_url"
+  },
+  "sinks": {
+    "s3": {
+      "bucket": "s3-bucket",
+      "prefix": "bucket_prefix"
+    }
+  }
+}
+      )
+      config = parse_json_configuration(body)
+
+      assert config[:sources][:gtfs_realtime] == %{
+               name_1: "url_1",
+               name_2: "url_2"
+             }
+
+      assert config[:sources][:gtfs_realtime_enhanced] == %{
+               enhanced_1: "url_3"
+             }
+
+      assert config[:gtfs][:url] == "gtfs_url"
+      assert config[:sinks][:s3][:bucket] == "s3-bucket"
+      assert config[:sinks][:s3][:prefix] == "bucket_prefix"
+    end
+
+    test "missing keys aren't configured" do
+      body = ~s(
+        {
+          "sources": {},
+          "sinks": {}
+        })
+      config = parse_json_configuration(body)
+      assert config[:sources][:gtfs_realtime] == %{}
+      assert config[:sources][:gtfs_realtime_enhanced] == %{}
+      assert config[:gtfs] == nil
+      assert config[:sinks] == %{}
+    end
+  end
+end
