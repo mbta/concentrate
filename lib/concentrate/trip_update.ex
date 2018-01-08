92acb69ab3d53eb26c97308573d55b7561230ce9
Paul Swartz <paul@paulswartz.net>
2018-01-08 12:52:15 -0500

    small merge speedups
    * don't look up the merge implementation multiple times
    * don't build a new object for merging: update the existing one

--- lib/concentrate/trip_update.ex
+++ lib/concentrate/trip_update.ex
@@ -17,19 +17,19 @@ defmodule Concentrate.TripUpdate do
     def key(%{trip_id: trip_id}), do: trip_id
 
     def merge(first, second) do
-      @for.new(
-        trip_id: first.trip_id,
-        route_id: first.route_id || second.route_id,
-        direction_id: first.direction_id || second.direction_id,
-        start_date: first.start_date || second.start_date,
-        start_time: first.start_time || second.start_time,
-        schedule_relationship:
-          if first.schedule_relationship == :SCHEDULED do
-            second.schedule_relationship
-          else
-            first.schedule_relationship
-          end
-      )
+      %{
+        first
+        | route_id: first.route_id || second.route_id,
+          direction_id: first.direction_id || second.direction_id,
+          start_date: first.start_date || second.start_date,
+          start_time: first.start_time || second.start_time,
+          schedule_relationship:
+            if first.schedule_relationship == :SCHEDULED do
+              second.schedule_relationship
+            else
+              first.schedule_relationship
+            end
+      }
     end
   end
 end
