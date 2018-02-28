0059bf0a6cf9bf8cbfce96ac796f500787771c19
Paul Swartz <paul@paulswartz.net>
2018-02-28 07:49:30 -0500

    update to Elixir 1.6.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Concentrate.MixProject do
     [
       app: :concentrate,
       version: "0.1.0",
-      elixir: "~> 1.5 or ~> 1.6-dev",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
