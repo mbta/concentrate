efcf76bdc657be4e574d44ff504a815ef1c826ae
Paul Swartz <paul@paulswartz.net>
2018-03-07 12:14:35 -0500

    add optional Encoder callback to use pre-grouped data

--- lib/concentrate/encoder.ex
+++ lib/concentrate/encoder.ex
@@ -3,6 +3,10 @@ defmodule Concentrate.Encoder do
   Encoders define a single callback:
 
   encode/1: given a list of parsed data, returns a binary
+  encode_groups/1: given a pre-grouped list of data, returns as binary
   """
   @callback encode([Concentrate.Parser.parsed()]) :: binary
+  @callback encode_groups([Concentrate.Encoder.GTFSRealtimeHelpers.trip_group()]) :: binary
+
+  @optional_callbacks encode_groups: 1
 end
