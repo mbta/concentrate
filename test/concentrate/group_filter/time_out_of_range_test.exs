4b3a2194d27b12a91e67b232ed520571ed6f866b
Paul Swartz <paul@paulswartz.net>
2018-03-09 16:47:17 -0500

    port TimeOutOfRange to GroupFilter

---
+++ test/concentrate/group_filter/time_out_of_range_test.exs
@@ -0,0 +1,40 @@
+defmodule Concentrate.GroupFilter.TimeOutOfRangeTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  import Concentrate.GroupFilter.TimeOutOfRange
+  alias Concentrate.StopTimeUpdate
+
+  defp now do
+    100
+  end
+
+  describe "filter/1" do
+    test "removes StopTimeUpdates if they're in the past or future" do
+      stu = StopTimeUpdate.new(arrival_time: 100)
+      assert {_, [], [^stu]} = filter({nil, [], [stu]}, &now/0)
+
+      stu = StopTimeUpdate.new(departure_time: 10_000)
+      assert {_, [], [^stu]} = filter({nil, [], [stu]}, &now/0)
+
+      stu = StopTimeUpdate.new(arrival_time: 4)
+      assert {_, [], []} = filter({nil, [], [stu]}, &now/0)
+
+      stu = StopTimeUpdate.new(arrival_time: 12_000)
+      assert {_, [], []} = filter({nil, [], [stu]}, &now/0)
+    end
+
+    test "keeps stop time update if a previous update was in the range" do
+      stus = [
+        StopTimeUpdate.new(arrival_time: 100),
+        StopTimeUpdate.new(arrival_time: 101)
+      ]
+
+      group = {nil, [], stus}
+      assert filter(group, &now/0) == group
+    end
+
+    test "other values are returned as-is" do
+      assert filter(:value, &now/0) == :value
+    end
+  end
+end
