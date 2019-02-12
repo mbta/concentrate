3c9fbb753dc46d658e42e008d3e54682149ee21b
Paul Swartz <paul@paulswartz.net>
2019-02-12 13:25:10 -0500

    merge data from two different VehiclePosition structs
    Previously, we'd take the latest struct, but that assumed that all the
    structs would have equivalent data. That's not the case in practice, so we
    need to merge like we do for the other types.

--- test/concentrate/vehicle_position_test.exs
+++ test/concentrate/vehicle_position_test.exs
@@ -5,11 +5,12 @@ defmodule Concentrate.VehiclePositionTest do
   alias Concentrate.Mergeable
 
   describe "Concentrate.Mergeable" do
-    test "merge/2 takes the latest of the two" do
-      first = new(last_updated: 1, latitude: 1, longitude: 1)
+    test "merge/2 takes the latest of the two positions" do
+      first = new(last_updated: 1, latitude: 1, longitude: 1, trip_id: "trip")
       second = new(last_updated: 2, latitude: 2, longitude: 2)
-      assert Mergeable.merge(first, second) == second
-      assert Mergeable.merge(second, first) == second
+      expected = new(last_updated: 2, latitude: 2, longitude: 2, trip_id: "trip")
+      assert Mergeable.merge(first, second) == expected
+      assert Mergeable.merge(second, first) == expected
     end
   end
 end
