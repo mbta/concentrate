21e47ab30655f5afc5856ebd4650e339d0324f93
Paul Swartz <paul@paulswartz.net>
2019-02-12 13:33:19 -0500

    merge data from two different VehiclePosition structs

--- lib/concentrate/vehicle_position.ex
+++ lib/concentrate/vehicle_position.ex
@@ -43,10 +43,29 @@ defmodule Concentrate.VehiclePosition do
 
     def merge(first, second) do
       if first.last_updated <= second.last_updated do
-        second
+        do_merge(first, second)
       else
-        first
+        do_merge(second, first)
       end
     end
+
+    defp do_merge(first, second) do
+      %{
+        second
+        | trip_id: first_value(second.trip_id, first.trip_id),
+          stop_id: first_value(second.stop_id, first.stop_id),
+          label: first_value(second.label, first.label),
+          license_plate: first_value(second.license_plate, first.license_plate),
+          latitude: first_value(second.latitude, first.latitude),
+          longitude: first_value(second.longitude, first.longitude),
+          bearing: first_value(second.bearing, first.bearing),
+          speed: first_value(second.speed, first.speed),
+          odometer: first_value(second.odometer, first.odometer),
+          stop_sequence: first_value(second.stop_sequence, first.stop_sequence)
+      }
+    end
+
+    defp first_value(value, _) when not is_nil(value), do: value
+    defp first_value(_, value), do: value
   end
 end
