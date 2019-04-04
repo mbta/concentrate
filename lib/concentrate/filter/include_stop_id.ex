93306aa3a7581d331293ccd7e858aecc5b9ac4c7
Paul Swartz <paul@paulswartz.net>
2019-04-04 14:35:48 -0400

    feat: fill in missing `stop_id` in StopTimeUpdates
    Resolves #74.

---
+++ lib/concentrate/filter/include_stop_id.ex
@@ -0,0 +1,34 @@
+defmodule Concentrate.Filter.IncludeStopID do
+  @moduledoc """
+  Adds missing stop IDs to StopTimeUpdates.
+  """
+  @behaviour Concentrate.Filter
+  alias Concentrate.Filter.GTFS.StopIDs
+  alias Concentrate.StopTimeUpdate
+
+  @impl Concentrate.Filter
+  def filter(item, stop_ids \\ StopIDs)
+
+  def filter(%StopTimeUpdate{} = stu, stop_ids) do
+    {:cont,
+     maybe_add_stop_id(
+       stu,
+       StopTimeUpdate.stop_id(stu),
+       StopTimeUpdate.trip_id(stu),
+       StopTimeUpdate.stop_sequence(stu),
+       stop_ids
+     )}
+  end
+
+  def filter(other, _stop_ids), do: {:cont, other}
+
+  defp maybe_add_stop_id(stu, nil, trip_id, stop_sequence, stop_ids)
+       when is_binary(trip_id) and is_integer(stop_sequence) do
+    case stop_ids.stop_id(trip_id, stop_sequence) do
+      :unknown -> stu
+      stop_id -> StopTimeUpdate.update_stop_id(stu, stop_id)
+    end
+  end
+
+  defp maybe_add_stop_id(stu, _, _, _, _), do: stu
+end
