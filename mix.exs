7239da719f4d71329cdd943fcf9c127d217df8c2
Paul Swartz <paul@paulswartz.net>
2019-05-13 13:14:20 -0400

    chore: update Elixir to 1.8.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Concentrate.MixProject do
     [
       app: :concentrate,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
