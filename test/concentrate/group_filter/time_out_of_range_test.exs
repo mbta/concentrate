43c963642386a9377c4d0c967bd00a832078ec63
Frank LaNasa <fjlanasa@Franks-MacBook-Pro.local>
2020-01-28 16:35:24 -0500

    refactor: increase the lookback time in TimeOutOfRange from one minute to 10 minutes

--- test/concentrate/group_filter/time_out_of_range_test.exs
+++ test/concentrate/group_filter/time_out_of_range_test.exs
@@ -5,12 +5,12 @@ defmodule Concentrate.GroupFilter.TimeOutOfRangeTest do
   alias Concentrate.StopTimeUpdate
 
   defp now do
-    100
+    1000
   end
 
   describe "filter/1" do
     test "removes StopTimeUpdates if they're in the past or future" do
-      stu = StopTimeUpdate.new(arrival_time: 100)
+      stu = StopTimeUpdate.new(arrival_time: 1000)
       assert {_, [], [^stu]} = filter({nil, [], [stu]}, &now/0)
 
       stu = StopTimeUpdate.new(departure_time: 10_000)
@@ -19,14 +19,17 @@ defmodule Concentrate.GroupFilter.TimeOutOfRangeTest do
       stu = StopTimeUpdate.new(arrival_time: 4)
       assert {_, [], []} = filter({nil, [], [stu]}, &now/0)
 
+      stu = StopTimeUpdate.new(arrival_time: 400)
+      assert {_, [], [^stu]} = filter({nil, [], [stu]}, &now/0)
+
       stu = StopTimeUpdate.new(arrival_time: 12_000)
       assert {_, [], []} = filter({nil, [], [stu]}, &now/0)
     end
 
     test "keeps stop time update if a previous update was in the range" do
       stus = [
-        StopTimeUpdate.new(arrival_time: 100),
-        StopTimeUpdate.new(arrival_time: 101)
+        StopTimeUpdate.new(arrival_time: 1000),
+        StopTimeUpdate.new(arrival_time: 120_000)
       ]
 
       group = {nil, [], stus}
