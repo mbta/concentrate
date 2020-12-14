283c2c96f709ac95157d57f55d91020d06899ebf
Cora Grant <394835+x@users.noreply.github.com>
2020-12-14 16:40:17 -0500

    chore: add Docker Hub login to build-push script

--- semaphore/build_push.sh
+++ semaphore/build_push.sh
@@ -7,6 +7,11 @@ set -e -x -u
 # * DOCKER_REPO
 awsenv=$1
 
+# log into docker hub if credentials are in the environment
+if [ -n "$DOCKER_USERNAME" ]; then
+  echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+fi
+
 # build docker image and tag it with git hash and aws environment
 githash=$(git rev-parse --short HEAD)
 docker build --pull -t $APP:latest .
