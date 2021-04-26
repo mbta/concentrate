0c686bae259ad3df7b9c8c0d21f8d3b46012e529
Paul Swartz <paul@paulswartz.net>
2021-04-26 12:10:53 -0400

    ci: remove Semaphore scripts
    Concentrate has migrated to GitHub Actions.

--- semaphore/dialyzer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e -x -u
-
-mkdir -p _build/dev
-find $SEMAPHORE_CACHE_DIR -name "dialyxir_*_deps-dev.plt*" | xargs -I{} cp '{}' _build/dev
-export ERL_CRASH_DUMP=/dev/null
-mix dialyzer --plt
-cp _build/dev/*_deps-dev.plt* $SEMAPHORE_CACHE_DIR
-mix dialyzer --halt-exit-status
--- semaphore/elixir_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e -x -u
-
-env MIX_ENV=test mix do compile --force --warnings-as-errors, test --cover
