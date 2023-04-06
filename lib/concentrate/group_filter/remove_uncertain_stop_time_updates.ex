7965831c841033da0ae2c243b431a50500b2b781
Beatrix Klebe <bklebe@mbta.com>
2023-04-06 17:05:42 -0400

    fixup! fix: handle nil TripDescriptors

--- lib/concentrate/group_filter/remove_uncertain_stop_time_updates.ex
+++ lib/concentrate/group_filter/remove_uncertain_stop_time_updates.ex
@@ -31,7 +31,7 @@ defmodule Concentrate.GroupFilter.RemoveUncertainStopTimeUpdates do
 
   def filter(group, exclusions) when exclusions == %{}, do: group
 
-  def filter({nil, _, _} = group, exclusions) when exclusions == %{}, do: group
+  def filter({nil, _, _} = group, _), do: group
 
   def filter({trip_descriptor, vehicle_positions, stop_time_updates}, exclusions) do
     route_id = TripDescriptor.route_id(trip_descriptor)
