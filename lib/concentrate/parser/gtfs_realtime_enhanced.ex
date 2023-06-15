6211755b6ba6231f122fb12bd6bec0c3ef062264
Nick Stein <nstein@mbta.com>
2023-06-15 13:10:02 -0400

    wip: removed unused

--- lib/concentrate/parser/gtfs_realtime_enhanced.ex
+++ lib/concentrate/parser/gtfs_realtime_enhanced.ex
@@ -238,8 +238,6 @@ defmodule Concentrate.Parser.GTFSRealtimeEnhanced do
 
   defp occupancy_status(nil), do: nil
 
-  defp multi_carriage_details(nil), do: nil
-
   for status <-
         ~w(EMPTY MANY_SEATS_AVAILABLE FEW_SEATS_AVAILABLE STANDING_ROOM_ONLY
            CRUSHED_STANDING_ROOM_ONLY FULL NOT_ACCEPTING_PASSENGERS)a do
