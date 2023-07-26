1b59e6fb7c59a027069c25018b2a09dc7db626ab
Walton Hoops <me@waltonhoops.com>
2023-07-26 11:22:57 -0600

    filter out STUs with no arrival/departure but are scheduled

---
+++ lib/concentrate/filter/unscheduled_scheduled_stop.ex
@@ -0,0 +1,25 @@
+defmodule Concentrate.Filter.UnscheduledScheduledStop do
+  @moduledoc """
+  Filters out StopTimeUpdates with a null arrival and departure time but a schedule_relationship of scheduled.
+  """
+  @behaviour Concentrate.Filter
+  alias Concentrate.StopTimeUpdate
+  require Logger
+
+  @impl Concentrate.Filter
+  def filter(%StopTimeUpdate{} = stu) do
+    if !StopTimeUpdate.arrival_time(stu) && !StopTimeUpdate.departure_time(stu) &&
+         (StopTimeUpdate.schedule_relationship(stu) == :SCHEDULED ||
+            !StopTimeUpdate.schedule_relationship(stu)) do
+      Logger.warning(
+        "event=unscheduled_scheduled_stop trip_id=#{stu.trip_id} stop_sequence=#{stu.stop_sequence} schedule_relationship=#{stu.schedule_relationship} status=#{stu.status} stu=#{inspect(stu)}"
+      )
+
+      {:skip, stu}
+    else
+      {:cont, stu}
+    end
+  end
+
+  def filter(other), do: {:cont, other}
+end
