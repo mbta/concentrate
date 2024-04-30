a8713856477f325ba2f0110a516bdf9a8e6f7485
Brian Fauble <bfauble@users.noreply.github.com>
2024-04-30 09:49:44 -0600

    add group filter for filtering out stus for stops that are currently flagged for suppression

---
+++ lib/concentrate/group_filter/supress_stop_time_update.ex
@@ -0,0 +1,48 @@
+defmodule Concentrate.GroupFilter.SupressStopTimeUpdate do
+  @moduledoc """
+  Filters out StopTimeUpdates with a null arrival and departure time but a schedule_relationship of scheduled.
+  """
+  @behaviour Concentrate.GroupFilter
+  alias Concentrate.Parser.StopPredictionStatus
+  alias Concentrate.StopTimeUpdate
+  alias Concentrate.TripDescriptor
+  require Logger
+
+  @impl Concentrate.GroupFilter
+  def filter({td, vps, stus}) do
+    case StopPredictionStatus.flagged_stops_on_route(td) do
+      nil ->
+        {td, vps, stus}
+
+      supressed_stops ->
+        route_id = TripDescriptor.route_id(td)
+        direction_id = TripDescriptor.direction_id(td)
+
+        unsupressed_stus =
+          Enum.reject(stus, fn stu ->
+            stop_id_supressed?(
+              supressed_stops,
+              StopTimeUpdate.stop_id(stu),
+              route_id,
+              direction_id
+            )
+          end)
+
+        {td, vps, unsupressed_stus}
+    end
+  end
+
+  def filter(other), do: other
+
+  defp stop_id_supressed?(supressed_stops, stop_id, route_id, direction_id) do
+    if MapSet.member?(supressed_stops, stop_id) do
+      Logger.info(fn ->
+        "Predictions for stop_id=#{stop_id} route_id=#{route_id} direction_id=#{direction_id} have been suppressed based on RTS feed trigger"
+      end)
+
+      true
+    else
+      false
+    end
+  end
+end
