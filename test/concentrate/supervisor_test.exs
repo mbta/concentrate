18d5863fe1d436ac516c356c049a642070fdc5df
Brian Fauble <bfauble@users.noreply.github.com>
2024-05-08 16:22:24 -0600

    fix test

--- test/concentrate/supervisor_test.exs
+++ test/concentrate/supervisor_test.exs
@@ -15,10 +15,10 @@ defmodule Concentrate.SupervisorTest do
 
   describe "children/1" do
     test "builds the right number of children" do
-      # currently, the right number is 5: HTTP pool, alerts, GTFS, pipeline, health
+      # currently, the right number is 6: HTTP pool, alerts, signs_stops_config, GTFS, pipeline, health
       actual = children([])
 
-      assert length(actual) == 5
+      assert length(actual) == 6
     end
   end
 end
