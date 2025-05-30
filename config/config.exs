ec42669e02d022e93027da677849bae2c6ca7a45
JD <jd@rudiejd.io>
2025-05-30 11:42:36 -0400

    fixup: change config

--- config/config.exs
+++ config/config.exs
@@ -69,7 +69,7 @@ config :concentrate,
     },
     {
       Concentrate.GroupFilter.RemoveUncertainStopTimeUpdates,
-      uncertainties_by_route: %{"Green-C" => [120, 360], "Green-D" => [120, 360]}
+      uncertainties_by_route: %{"Green-C" => %{120 => [1], 360 => [1]}, "Green-D" => %{120 => [1], 360 => [1]}}
     },
     Concentrate.GroupFilter.TimeOutOfRange,
     Concentrate.GroupFilter.RemoveUnneededTimes,
