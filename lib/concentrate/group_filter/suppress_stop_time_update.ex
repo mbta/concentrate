c334bcbbe873bad0a9a60ceb07edc04428e2118c
Walton Hoops <me@waltonhoops.com>
2025-06-30 14:30:53 -0600

    fix: stop supression no affecting terminal predictions and terminal predictions not supressing all STUs

--- lib/concentrate/group_filter/suppress_stop_time_update.ex
+++ lib/concentrate/group_filter/suppress_stop_time_update.ex
@@ -14,30 +14,57 @@ defmodule Concentrate.GroupFilter.SuppressStopTimeUpdate do
       )
 
   def filter({td, vps, stus}, module) do
-    route_id = TripDescriptor.route_id(td)
-    direction_id = TripDescriptor.direction_id(td)
-    update_type = TripDescriptor.update_type(td)
+    suppressed_terminals =
+      module.terminals_suppressed(TripDescriptor.route_id(td), TripDescriptor.direction_id(td))
 
-    case module.flagged_stops_on_route(route_id, direction_id, update_type) do
-      [] ->
-        {td, vps, stus}
+    suppressed_stops =
+      module.suppressed_stops_on_route(
+        TripDescriptor.route_id(td),
+        TripDescriptor.direction_id(td)
+      )
 
-      suppressed_stops ->
-        {suppressed_stus, unsuppressed_stus} =
-          Enum.split_with(stus, fn stu ->
-            stop_id_suppressed?(
-              suppressed_stops,
-              StopTimeUpdate.stop_id(stu)
-            )
-          end)
+    stus =
+      stus
+      |> perform_terminal_suppression(td, suppressed_terminals)
+      |> perform_stop_suppression(td, suppressed_stops)
 
-        log_suppressed_stus(suppressed_stus, route_id, direction_id)
-
-        {td, vps, unsuppressed_stus}
-    end
+    {td, vps, stus}
   end
 
   def filter(other, _), do: other
+
+  defp perform_terminal_suppression(stus, td, suppressed_terminals) do
+    first_stu = List.first(stus)
+
+    if TripDescriptor.update_type(td) in ["at_terminal", "reverse_trip"] && first_stu &&
+         stop_id_suppressed?(suppressed_terminals, StopTimeUpdate.stop_id(first_stu)) do
+      Logger.info(
+        "event=terminal_predictions_suppression route_id=#{TripDescriptor.route_id(td)} direction_id=#{TripDescriptor.direction_id(td)} have been suppressed based on Screenplay API trigger"
+      )
+
+      []
+    else
+      stus
+    end
+  end
+
+  defp perform_stop_suppression(stus, td, suppressed_stops) do
+    {suppressed_stus, unsuppressed_stus} =
+      Enum.split_with(stus, fn stu ->
+        stop_id_suppressed?(
+          suppressed_stops,
+          StopTimeUpdate.stop_id(stu)
+        )
+      end)
+
+    log_suppressed_stus(
+      suppressed_stus,
+      TripDescriptor.route_id(td),
+      TripDescriptor.direction_id(td)
+    )
+
+    unsuppressed_stus
+  end
 
   defp stop_id_suppressed?(suppressed_stops, stop_id) do
     case Stops.parent_station_id(stop_id) do
@@ -50,9 +77,9 @@ defmodule Concentrate.GroupFilter.SuppressStopTimeUpdate do
     Enum.each(stus, fn stu ->
       stop_id = StopTimeUpdate.stop_id(stu)
 
-      Logger.info(fn ->
-        "Predictions for stop_id=#{stop_id} route_id=#{route_id} direction_id=#{direction_id} have been suppressed based on Screenplay API trigger"
-      end)
+      Logger.info(
+        "Predictions for stop_id=\"#{stop_id}\" route_id=#{route_id} direction_id=#{direction_id} have been suppressed based on Screenplay API trigger"
+      )
     end)
   end
 end
--- test/concentrate/group_filter/suppress_stop_time_update_test.exs
+++ test/concentrate/group_filter/suppress_stop_time_update_test.exs
@@ -7,8 +7,13 @@ defmodule Concentrate.GroupFilter.SuppressStopTimeUpdateTest do
   alias Concentrate.{StopTimeUpdate, TripDescriptor}
 
   defmodule FakeStopPredictionStatus do
-    def flagged_stops_on_route("Red", 0, _), do: MapSet.new(["place-hymnl", "70086"])
-    def flagged_stops_on_route(_, _, _), do: []
+    def suppressed_stops_on_route("Red", 0), do: MapSet.new(["place-hymnl"])
+    def suppressed_stops_on_route("Red", 1), do: MapSet.new([])
+    def suppressed_stops_on_route(_, _), do: MapSet.new()
+
+    def terminals_suppressed("Red", 0), do: MapSet.new(["70086"])
+    def terminals_suppressed("Red", 1), do: MapSet.new(["place-hymnl"])
+    def terminals_suppressed(_, _), do: MapSet.new()
   end
 
   describe "filter/1" do
@@ -18,7 +23,7 @@ defmodule Concentrate.GroupFilter.SuppressStopTimeUpdateTest do
       Stops._insert_mapping("70086", "place-jfk")
     end
 
-    test "removes stop_time_updates of the corresponding trip_id + direction_id + stop_id combos if they are currently flagged and logs a message" do
+    test "removes stop_time_updates based on stop suppressions and logs a message" do
       td = TripDescriptor.new(route_id: "Red", direction_id: 0, update_type: "mid_trip")
 
       stu1 = StopTimeUpdate.new(stop_id: "70152")
@@ -33,23 +38,93 @@ defmodule Concentrate.GroupFilter.SuppressStopTimeUpdateTest do
 
       log =
         capture_log(fn ->
-          assert {^td, [], [^stu3]} =
+          assert {^td, [], [^stu2, ^stu3]} =
                    SuppressStopTimeUpdate.filter({td, [], stus}, FakeStopPredictionStatus)
         end)
 
       assert log =~
-               "Predictions for stop_id=70152 route_id=Red direction_id=0 have been suppressed based on Screenplay API trigger"
+               "Predictions for stop_id=\"70152\" route_id=Red direction_id=0 have been suppressed based on Screenplay API trigger"
+
+      refute log =~ "stop_id=70086"
+      refute log =~ "stop_id=70053"
+    end
+
+    test "removes all stop_time_updates for terminal suppression when update_type is 'at_terminal'" do
+      td = TripDescriptor.new(route_id: "Red", direction_id: 0, update_type: "at_terminal")
+
+      stu1 = StopTimeUpdate.new(stop_id: "70086")
+      stu2 = StopTimeUpdate.new(stop_id: "70152")
+      stu3 = StopTimeUpdate.new(stop_id: "70053")
+
+      stus = [
+        stu1,
+        stu2,
+        stu3
+      ]
+
+      log =
+        capture_log(fn ->
+          assert {^td, [], []} =
+                   SuppressStopTimeUpdate.filter({td, [], stus}, FakeStopPredictionStatus)
+        end)
 
       assert log =~
-               "Predictions for stop_id=70086 route_id=Red direction_id=0 have been suppressed based on Screenplay API trigger"
+               "event=terminal_predictions_suppression route_id=Red direction_id=0 have been suppressed based on Screenplay API trigger"
+    end
+
+    test "removes all stop_time_updates for terminal suppression when update_type is 'reverse_trip'" do
+      td = TripDescriptor.new(route_id: "Red", direction_id: 1, update_type: "reverse_trip")
+
+      stu1 = StopTimeUpdate.new(stop_id: "70152")
+      stu2 = StopTimeUpdate.new(stop_id: "70086")
+      stu3 = StopTimeUpdate.new(stop_id: "70053")
+
+      stus = [
+        stu1,
+        stu2,
+        stu3
+      ]
+
+      log =
+        capture_log(fn ->
+          assert {^td, [], []} =
+                   SuppressStopTimeUpdate.filter({td, [], stus}, FakeStopPredictionStatus)
+        end)
+
+      assert log =~
+               "event=terminal_predictions_suppression route_id=Red direction_id=1 have been suppressed based on Screenplay API trigger"
+    end
+
+    test "does not remove stop_time_updates for terminal suppression when first stop is not a suppressed terminal" do
+      td = TripDescriptor.new(route_id: "Red", direction_id: 0, update_type: "at_terminal")
+
+      # Not a terminal stop
+      stu1 = StopTimeUpdate.new(stop_id: "70053")
+      stu2 = StopTimeUpdate.new(stop_id: "70086")
+      stu3 = StopTimeUpdate.new(stop_id: "70152")
+
+      stus = [
+        stu1,
+        stu2,
+        stu3
+      ]
+
+      log =
+        capture_log(fn ->
+          assert {^td, [], [^stu1, ^stu2]} =
+                   SuppressStopTimeUpdate.filter({td, [], stus}, FakeStopPredictionStatus)
+        end)
+
+      refute log =~ "event=terminal_predictions_suppression"
+      assert log =~ "Predictions for stop_id=\"70152\""
     end
 
     test "requires route_id to filter out stus" do
       td = TripDescriptor.new(route_id: nil, direction_id: 0, update_type: "mid_trip")
 
-      stu1 = StopTimeUpdate.new(stop_id: 0)
-      stu2 = StopTimeUpdate.new(stop_id: 123)
-      stu3 = StopTimeUpdate.new(stop_id: 4)
+      stu1 = StopTimeUpdate.new(stop_id: "0")
+      stu2 = StopTimeUpdate.new(stop_id: "123")
+      stu3 = StopTimeUpdate.new(stop_id: "4")
 
       stus = [
         stu1,
@@ -69,9 +144,9 @@ defmodule Concentrate.GroupFilter.SuppressStopTimeUpdateTest do
     test "requires direction_id to filter out stus" do
       td = TripDescriptor.new(route_id: "Red", direction_id: nil, update_type: "mid_trip")
 
-      stu1 = StopTimeUpdate.new(stop_id: 0)
-      stu2 = StopTimeUpdate.new(stop_id: 123)
-      stu3 = StopTimeUpdate.new(stop_id: 4)
+      stu1 = StopTimeUpdate.new(stop_id: "0")
+      stu2 = StopTimeUpdate.new(stop_id: "123")
+      stu3 = StopTimeUpdate.new(stop_id: "4")
 
       stus = [
         stu1,
